b9bf5affe17f9ba6221ecce64cc13e502337aea0
sakhadib <sakhadib@gmail.com>
2025-12-29 17:21:04 +0600

    ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Task;
-use App\Observers\TaskObserver;
-
 use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // URL::forceRootUrl(config('app.url'));
-        // URL::forceScheme('https');
-        Task::observe(TaskObserver::class);
+        if(config('app.url')){
+            URL::forceRootUrl(config('app.url'));
+            URL::forceScheme('https');
+        }
     }
 }
